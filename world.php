75f03759c0c8ea93f00f8d68dec543965eefa6c7
Brittanie Morgan <brittaniemorgan12345@gmail.com>
2022-11-12 18:35:07 -0500

    Exercise 4 Completed: Table added

--- world.php
+++ world.php
@@ -13,10 +13,26 @@ if (isset($_GET[htmlentities("country")])){
   $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
-
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<table border="1px">  
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Continent</th>
+      <th>Independence</th>
+      <th>Head of State</th>
+  </tr>  
+  </thead>
+
+  <?php foreach ($results as $row): ?>
+  <tbody>
+    <tr>
+      <td><?=$row["name"]?></td> 
+      <td><?=$row["continent"]?></td>
+      <td><?=$row["independence_year"]?></td>
+      <td><?=$row["head_of_state"]?></td>
+    </tr>
+  </tbody>
+  <?php endforeach; ?>
+</table>
+
